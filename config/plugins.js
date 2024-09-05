5b3892b5b10467ed68558af3ab66f1accb07f51f
Pau Romeu Bruguera <pau.romeu.bruguera@gmail.com>
2024-09-05 23:43:23 +0200

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,16 @@
-module.exports = {
-    'strapi-neon-tech-db-branches': {
-        enabled: true,
-        config: {
-            neonApiKey: "l476j8yu0ytypg5g9lc885dk9qfwhqtx313wf2n7dwvd6toqngej1tc1prg4kmxv", // get it from here: https://console.neon.tech/app/settings/api-keys
-            neonProjectName: "masmineirons", // the neon project under wich your DB runs
-            neonRole: "masmineironsdb_owner", // create it manually under roles for your project first
-            gitBranch: "main", // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
         },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
     },
-};
+  });
